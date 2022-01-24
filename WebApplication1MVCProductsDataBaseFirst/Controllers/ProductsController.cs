1dc37d93b171df477d7c788abae31e47f935ebc7
samikshya.sibani <samikshya.sibani@gmail.com>
2022-01-24 21:24:54 +0530

    Add project files.

---
+++ WebApplication1MVCProductsDataBaseFirst/Controllers/ProductsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1MVCProductsDataBaseFirst.Models;
+
+namespace WebApplication1MVCProductsDataBaseFirst.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ProductsMVCEntities db = new ProductsMVCEntities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var tblProducts = db.tblProducts.Include(t => t.Brand).Include(t => t.Category);
+            return View(tblProducts.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblProduct tblProduct = db.tblProducts.Find(id);
+            if (tblProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblProduct);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.Brand_ID = new SelectList(db.Brands, "ID", "BrandName");
+            ViewBag.Category_ID = new SelectList(db.Categories, "ID", "CategoryName");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Category_ID,Brand_ID,ProductName,Quantity,price")] tblProduct tblProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblProducts.Add(tblProduct);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Brand_ID = new SelectList(db.Brands, "ID", "BrandName", tblProduct.Brand_ID);
+            ViewBag.Category_ID = new SelectList(db.Categories, "ID", "CategoryName", tblProduct.Category_ID);
+            return View(tblProduct);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblProduct tblProduct = db.tblProducts.Find(id);
+            if (tblProduct == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Brand_ID = new SelectList(db.Brands, "ID", "BrandName", tblProduct.Brand_ID);
+            ViewBag.Category_ID = new SelectList(db.Categories, "ID", "CategoryName", tblProduct.Category_ID);
+            return View(tblProduct);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Category_ID,Brand_ID,ProductName,Quantity,price")] tblProduct tblProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblProduct).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Brand_ID = new SelectList(db.Brands, "ID", "BrandName", tblProduct.Brand_ID);
+            ViewBag.Category_ID = new SelectList(db.Categories, "ID", "CategoryName", tblProduct.Category_ID);
+            return View(tblProduct);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblProduct tblProduct = db.tblProducts.Find(id);
+            if (tblProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblProduct);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblProduct tblProduct = db.tblProducts.Find(id);
+            db.tblProducts.Remove(tblProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
